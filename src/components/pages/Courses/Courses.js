71f5800d811d6b49e2ef2cc679108b85a7dfb99a
SarathAdhi <s.sarathadhithya@gmail.com>
2022-01-31 20:05:02 +0530

    update

--- src/components/pages/Courses/Courses.js
+++ src/components/pages/Courses/Courses.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import './courses.css';
-import { CourseLists } from '../../data/courses';
+
+const url = "https://book-your-slot-api.herokuapp.com/CourseLists";
 
 function Courses() {
 
-    
+    fetch(url)
+    .then(res => res.json())
+    .then(data=> {
+        console.log(data.length)
+        for(var i=0; i<data.length; i++) {
+
+            var heading = document.createElement('h1');
+            heading.innerHTML = data[i].id + ". " + data[i].cname;
+
+
+            var h1 = document.querySelector('.course-list')
+
+            h1.appendChild(heading)
+
+        }  
+    })
 
     return (
         <div className='courses'>
             <div className='courses-container'>
                 <h1 className='heading'>Courses</h1>
                 <div className='course-list'>
-                    { 
-                        CourseLists.map((index)=>(
                             
-                        <h3>{index.id + "). " + index.cname}</h3>
-                     )) }
                 </div>
             </div>
         </div>
